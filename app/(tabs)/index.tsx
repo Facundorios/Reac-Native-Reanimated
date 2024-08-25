d312eff064b2b41fdeaf39733257b1e8fccf4e38
Facundo Ríos <Facundorios2005@gmail.com>
2024-08-25 00:07:12 -0300

    feat: :art:

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,26 +9,36 @@ import {
 } from "react-native-reanimated";
 
 export default function HomeScreen() {
-  const slide = useSharedValue(-2050);
+  const slide = useSharedValue(-1000);
+  const fade = useSharedValue(0);
 
   useEffect(() => {
     slide.value = withTiming(0, { duration: 2500 });
+    fade.value = withTiming(1, { duration: 2500 });
   });
 
   const animatedStyle = useAnimatedStyle(() => ({
     transform: [{ translateY: slide.value }],
   }));
 
+  const handlePress = () => {
+    fade.value = withTiming(fade.value === 0 ? 1 : 0, { duration: 500 });
+  };
+
+  const backgroundStyle = useAnimatedStyle(() => ({
+    backgroundColor: fade.value === 0 ? "red" : "blue",
+  }));
+
   return (
     <>
-      <Animated.View>
+      <Animated.View style={[styles.backgorund, backgroundStyle]}>
         <Animated.View style={animatedStyle}>
           <Text style={[styles.components, animatedStyle]}>
             Este Proyecto contiene React-Native-Reanimated
           </Text>
         </Animated.View>
         <Pressable style={styles.button}>
-          <Button title="Iniciar" />
+          <Button title="Iniciar" onPress={handlePress} />
         </Pressable>
       </Animated.View>
     </>
@@ -36,6 +46,11 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
+  backgorund: {
+    flex: 1,
+    width: "100%",
+  },
+
   components: {
     alignItems: "center",
     fontSize: 26,
